7267c638fcdd9897365ae1b2dfc8081067e314e2
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-20 19:28:54 +0300

    SEM01

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+    Console.WriteLine("число является четным");
+else Console.WriteLine("число является нечетным");
