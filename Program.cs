9e6b85afae39c9791d3d62b4700f2a6da1882ed8
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-20 18:40:06 +0300

    SEM01

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 < num2)
+    max = num2;
+if (num2 < num3)
+    max = num3;
+Console.WriteLine(num1+", " + num2+ ", "+ num3+ " -> " +max);
