3b9ce9eaab64fefe8488c7905f6271c3cc788990
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-21 14:43:18 +0300

    SEM01

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int current = 1;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (current <= num)
+    if (current % 2 == 0)
+        Console.Write(current + "");
+    current++;
