a565aeef14ae05139fba4a562a7b2d8e61a213a3
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-25 20:29:38 +0300

    SEM02

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿int Sec(int Number){
+    int n = Number/10%10;
+    return n;}
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = Sec(num);
+Console.WriteLine(num + " -> " + a);
