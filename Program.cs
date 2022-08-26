69307e0b8d3ac6c280be1adc43221b84634adf6f
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-26 21:43:42 +0300

    SEM02

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// void Days(){
+// Console.WriteLine("введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num <1|| num > 7){
+// Console.WriteLine(num + " некорректный день");}
+// else if(num > 5){
+// Console.WriteLine(num + " выходной день");}
+// else {
+// Console.WriteLine(num + " рабочий день");}
+// }
+// Days();
