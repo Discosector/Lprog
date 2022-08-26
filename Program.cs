fa03f31b6688a5b0e8b4fe670fdd1bd51aeb621e
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-26 22:03:14 +0300

    SEM02

--- Program.cs
+++ Program.cs
@@ -1,28 +1,15 @@
-﻿// int Three(int Number){
-// while (Number > 999){
-//     Number = Number/10;
-// }
-//     int n = Number%10;
-//     return n;}
-// Console.WriteLine("Введите число:");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int a = Three (num);
-// Console.WriteLine(a);
-
-
-
-// void Three(){
+﻿// void Three(){
 // Console.WriteLine("Введите число:");
 // int Number = Convert.ToInt32(Console.ReadLine());
-// if (Number < 100);
-//    Console.WriteLine("третьей цифры нет")
-// else
+// if (Number < 100)
+//    Console.WriteLine("третьей цифры нет");
+// else 
 // {
-//     while (Number > 999){
+//     while (Number > 999)
 //     Number = Number/10;
 // }
 //     int n = Number%10;
 //     Console.WriteLine(n);
 // }
-// }
+
 // Three();
