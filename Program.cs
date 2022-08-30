97ad0b74bf55e04585696a82ddafeca4509d76d0
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-30 23:35:21 +0300

    SEM03

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// double Dist(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+// }
+// Console.WriteLine("введите координату X1: ");
+// int xA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите координату Y1: ");
+// int yA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите координату Z1: ");
+// int zA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите координату X2: ");
+// int xB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите координату Y2: ");
+// int yB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите координату Z2: ");
+// int zB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("длина отрезка: " + Dist (xA, yA, zA, xB, yB, zB));
