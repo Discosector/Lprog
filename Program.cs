4e75ad6430269d797b33b485066c7b311a2d986d
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-30 23:48:06 +0300

    SEM03

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// void list(int n)
+// {
+// int count = 1;
+// Console.Write(n + "-> ");
+// if (n > 0)
+// {
+// while (count <= n)
+// {
+// Console.Write(Math.Pow(count, 3) + ", ");
+// count++;
+// }
+// }
+// else
+// {
+// count = -1;
+// while (count >= n)
+// {
+// Console.Write(Math.Pow(count, 3) + ", ");
+// count--;
+// }}}
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// list(num);
