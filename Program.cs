b49d6ff1608457da198edf7a1e490a520f1975a8
Dmitri Buchnev <skyfaller@mail.ru>
2022-08-31 01:24:10 +0300

    SEM03

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// int Pal(int num)
+// {
+// int a1 = num % 10;
+// int a2 = num / 10 % 10;
+// int a3 = num / 100 % 10;
+// int a4 = num / 1000 % 10;
+// int a5 = num / 10000%10;
+// int x = a1 * 10000 + a2 * 1000 + a3 * 100 + a4 * 10 + a5;
+//     return x;
+// }
+// Console.WriteLine("Введите пятизначное число: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+// int b = Pal(Number);
+// Console.WriteLine(b);
+//     if (b == Number)
+// {
+//     Console.WriteLine("палиндром");
+// }   
+// else {Console.WriteLine("не палиндром");}
+
+
