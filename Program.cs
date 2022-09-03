f1534d02f6dd8c24ff794479dc8177fa94aa4a48
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-03 20:26:24 +0300

    SEM04

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+
+// int [] CreArray (int size, int min, int max)
+// {
+//     int [] array = new int [size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array [i] = new Random().Next(min, max);
+//     }
+//     return array;
+// }
+// void ShowArray (int [] array){
+//     for (int i = 0; i < array.Length; i ++)
+//     {
+//         Console.Write(array [i] + " ");
+//     }
+// }
+// int [] myArray = CreArray (8, 0, 100);
+// ShowArray (myArray);
