5767039967577d6945cf34b069dcc4bd8bb72fe3
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-06 23:59:08 +0300

    SEM05

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+
+
+// Double[] CreArray(int size)
+// {
+//     double[] array = new double[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(1,100);
+//     }
+//     return array;
+// }
+// void ShowArray(double[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// double[] myArray = CreArray(10);
+// ShowArray(myArray);
+
+// double max = myArray[0];
+
+//     for (int i = 0; i < myArray.Length; i++)
+// {
+//     if (myArray[i]>max)
+//         { 
+//             max = myArray[i];
+//         }
+
+// }
+// Console.WriteLine ();
+// Console.WriteLine (max);
+
+// double min = myArray[0];
+
+//     for (int i = 0; i < myArray.Length; i++)
+// {
+//     if (myArray[i]<min)
+//         { 
+//             min = myArray[i];
+//         }
+
+// }
+
+// Console.WriteLine (min);
+
+// double dif = max - min;
+// Console.WriteLine ($"Разница между большим и меньшим: {dif}");
+
