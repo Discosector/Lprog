a537b5117b3ba85a8c0c5a9c6cf7745d2c27be3d
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-10 22:36:14 +0300

    SEM06

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+// Console.WriteLine("введите некие четыре значения");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// double k2 = Convert.ToInt32(Console.ReadLine());
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// double PointXY(double k1, double b1, double k2, double b2)
+// {
+
+//     if (k1 == k2)
+//     {
+//         return 1;
+//     }
+//     else 
+//     {
+//          return 0;
+//           }
+// }
+// if (PointXY(k1, b1, k2, b2)==1)
+// {
+//     Console.WriteLine("прямые параллельны");
+// }
+// else {  
+
+//     double xA =((b2-b1)/(k1-k2));
+//     double yA = k2*xA + b2;
+// Console.WriteLine ( xA + ";" + yA);
+// }
