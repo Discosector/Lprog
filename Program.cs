7cf3f42fd06e756c25c1bcb5763f2a0083e97026
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-10 21:05:16 +0300

    SEM06

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+// int[] CreArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         Console.Write("введите "+ i + " элемент: ");
+//         array [i] = Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// int[] myArray = CreArray(5);
+// ShowArray(myArray);
+
+// int count = 0;
+// for (int i = 0; i < myArray.Length; i++)
+// { if (myArray [i] > 0)
+//     count ++;
+// }
+// Console.Write($"колличество элементов больше ноля: {count}");
