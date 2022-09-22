2ba8103da82f9c9eb85bf4412c48abbe3d9e8921
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-22 20:11:20 +0300

    SEM09

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии
+
+// string PrintNumbers (int m, int n)
+// {
+//     if (m == n) return m.ToString ();
+// return (m + " " + PrintNumbers(m+1,n));
+// }
+// Console.Write ("введите первое число: ");
+// int M = int.Parse (Console.ReadLine());
+// Console.Write ("введите второе число: ");
+// int N = int.Parse (Console.ReadLine());
+// Console.WriteLine (PrintNumbers (M, N));
