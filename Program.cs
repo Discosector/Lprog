55c6408155301ed239b4c6dd95983fff1c10b3fa
Dmitri Buchnev <skyfaller@mail.ru>
2022-09-22 20:33:15 +0300

    SEM09

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 68: Задайте значения M и N. Напишите программу, которая найдёт наибольший общий делитель (НОД) этих чисел с помощью рекурсии
+
+// int NOD (int m, int n)
+// {
+//     if (n == 0) return m;
+// return (NOD (n,m % n));
+// }
+// Console.Write ("введите первое число: ");
+// int M = int.Parse (Console.ReadLine());
+// Console.Write ("введите второе число: ");
+// int N = int.Parse (Console.ReadLine());
+// Console.WriteLine (NOD (M, N));
